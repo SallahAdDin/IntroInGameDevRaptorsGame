48de7209022116c58f152dfedb1edc0b54bdc919
SallahAdDin <RomaOgo@mail.ru>
2016-09-18 20:51:26 +0300

    0001 add Player object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameMaster : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
